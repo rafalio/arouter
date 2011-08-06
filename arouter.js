5c7958901f4c9195813bce923e08ed6be3f19934
Rafal Szymanski <me@rafal.io>
2011-08-06 21:57:29 +0100

    Some updates

--- arouter.js
+++ arouter.js
@@ -1,31 +1,29 @@
-(function() {
-  exports.route = function(routes, app, middleware) {
-    var args, func, funcs, path, type, _results;
-    funcs = {
-      post: app.post,
-      get: app.get,
-      all: app.all
-    };
-    _results = [];
-    for (type in funcs) {
-      func = funcs[type];
-      _results.push((function() {
-        var _results2;
-        _results2 = [];
-        for (path in routes[type]) {
-          args = routes[type][path];
-          if (!Array.isArray(args)) {
-            args = [args];
-          }
-          if (middleware) {
-            args.unshift(middleware);
-          }
-          args.unshift(path);
-          _results2.push(func.apply(app, args));
-        }
-        return _results2;
-      })());
-    }
-    return _results;
+exports.route = function(routes, app, middleware) {
+  var args, func, funcs, path, type, _results;
+  funcs = {
+    post: app.post,
+    get: app.get,
+    all: app.all
   };
-}).call(this);
+  _results = [];
+  for (type in funcs) {
+    func = funcs[type];
+    _results.push((function() {
+      var _results2;
+      _results2 = [];
+      for (path in routes[type]) {
+        args = routes[type][path];
+        if (!Array.isArray(args)) {
+          args = [args];
+        }
+        if (middleware) {
+          args.unshift(middleware);
+        }
+        args.unshift(path);
+        _results2.push(func.apply(app, args));
+      }
+      return _results2;
+    })());
+  }
+  return _results;
+};
